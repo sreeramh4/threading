f6ecdb5e7f7db6d408d523e0e2d54456b7bba957
sreeramh4 <29130266+sreeramh4@users.noreply.github.com>
2017-11-30 23:34:46 +0530

    3.Infinite loop
    The function which the thread object executes has an infinite loop.
    Nothing happens infinitely. XD

--- 1intro.py
+++ 1intro.py
@@ -5,6 +5,10 @@ import threading
 def do_this():
     print "This is the second thread!"
     
+    #Creating an infinite loop
+    while(True):
+        pass    #Infinite loop, so only this thread is working now. Infinitely doing nothing.
+    
 def main():
     #creating a new thread object
     new_thread = threading.Thread (target=do_this)
