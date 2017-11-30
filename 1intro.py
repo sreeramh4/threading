2c7f3f8acb0692d414083d1102b6d0f8dcb3a467
sreeramh4 <29130266+sreeramh4@users.noreply.github.com>
2017-11-30 22:17:26 +0530

    Code from Vid 1 added.

---
+++ 1intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import threading
+
+def main():
+
+    print threading.active_count()
+    print threading.enumerate()
+    print threading.current_thread()
+    
+if ( __name__=="__main__"):
+    main()
