9fc7fe7229893ec89cb68620d65dcc4d9c078386
sreeramh4 <29130266+sreeramh4@users.noreply.github.com>
2017-11-30 23:42:45 +0530

    4. Terminating the infinite loop
    Terminating the infinite loop. The hard way.

--- 1intro.py
+++ 1intro.py
@@ -5,11 +5,18 @@ import threading
 def do_this():
     print "This is the second thread!"
     
+    global dead # initialising flag in this function, Note that the value will be updated in real time.
+    
     #Creating an infinite loop
-    while(True):
-        pass    #Infinite loop, so only this thread is working now. Infinitely doing nothing.
+    while( not dead):
+        pass    #Infinite loop, Infinitely does nothing.
     
 def main():
+    
+    global dead     #Creating a flag of sorts to exit the infinite loop
+    
+    dead = False    #Making the initial value False , so the other thread won't go into infinite loop initially.
+    
     #creating a new thread object
     new_thread = threading.Thread (target=do_this)
     #target() : specifies what to execute on making the object
@@ -18,6 +25,9 @@ def main():
     print threading.active_count()
     print threading.enumerate()
     print threading.current_thread()
+    
+    raw_input ("Hit enter to die")
+    dead = True     #Updates dead to True GLOBALLY.
     
 if ( __name__=="__main__"):
     main()
