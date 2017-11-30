2ced54c08e0d4e29a7593db4cec85ff57f6359e9
sreeramh4 <29130266+sreeramh4@users.noreply.github.com>
2017-11-30 23:08:34 +0530

    Thread creation
    Created a new thread and made it execute a function containing a print statement.

--- 1intro.py
+++ 1intro.py
@@ -2,8 +2,15 @@
 
 import threading
 
+def do_this():
+    print "This is the second thread!"
+    
 def main():
-
+    #creating a new thread object
+    new_thread = threading.Thread (target=do_this)
+    #target() : specifies what to execute on making the object
+    new_thread.start() # Executes new_thread.run
+    #Runs the new_thread object we made
     print threading.active_count()
     print threading.enumerate()
     print threading.current_thread()
